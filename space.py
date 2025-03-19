3972d868f74778694d8d861157353052276540b1
sarah <student.189241@worcesterschools.net>
2025-03-19 12:58:08 -0400

    move left and right

--- space.py
+++ space.py
@@ -1,9 +1,10 @@
-import pygame, sys
+import pygame, sys, random
 pygame.init()
 
 WINDOW_HEIGHT = 800
 WINDOW_WIDTH = 600
 screen = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH))
+background = pygame.image.load("bg.jpg")
 pygame.display.set_caption("Space Invaders")
 
 #Icon of game
@@ -14,10 +15,20 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load("spaceship64.png")
 playerX = 370
 playerY = 480 
-#modify player function to accept x coordinates
-def player(x, y):
-          screen.blit(playerImg, (x, y))
 
+#Enemy settings
+enemyX = random.randint(0, 800 - 64)
+enemyY = random.randint(50 ,150 )
+enemyX_momentum = .05 # enemy starts moving right
+enemyY_momentum = 10 
+enemyImg = pygame.image.load("alien64 (1).png")
+#modify player function to accept x coordinates
+#blit draws the player on the screen 
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 looping = True
 while looping:
@@ -26,12 +37,12 @@ while looping:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    
-    pygame.display.update()#updates screens
-    screen.fill((245, 200, 255))#light purple
+
+    screen.fill((0,0,0))
+    screen.blit(background, (0,0))
 #PLAYER MOVEMENT
 #get_pressed returns keys on the keyboard
-    player(playerX, playerY)
+
 
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_LEFT]:
@@ -43,8 +54,22 @@ while looping:
         playerX = 736
     elif playerX <= 0:
         playerX = 0
+
+
+    enemyX += enemyX_momentum
+
+    #enemy boundaries
+    if enemyX >= 736:
+        enemyX_momentum = -.07
+        enemyY += enemyY_momentum
+    elif enemyX <= 0:
+        enemyX_momentum = .07
+        enemyY += enemyY_momentum
     
     
 
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
   
-
+    
+    pygame.display.update()#updates screens
